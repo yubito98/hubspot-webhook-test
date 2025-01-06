ea05501a062819dd6e2bc9ee0e1ac9742957b7d4
Yubor Molina <yubor98@gmail.com>
2025-01-06 15:32:02 -0500

    Setting vercel api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+const axios = require("axios");
+const express = require("express");
+const cors = require('cors')
+const port = process.env.PORT || 8080;
+
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+
+
+app.get("/", (req, res) =>{
+    res.send("it is working")
+})
+
+
+
+app.listen(port, () =>{
+    console.log(`http://localhost:${port}`)
+})
